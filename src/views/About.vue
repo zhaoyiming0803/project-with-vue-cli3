1588a53cce89a44accfb9151d220df0a7a9ce51d
zhaoyiming <yiming_zhao@qingsongchou.com>
2019-08-16 18:12:22 +0800

    test

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,28 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script scoped>
+  export default {
+    data () {
+      return {
+
+      }
+    },
+
+    methods: {
+      async fn1 () {
+        const res = await this.fn2();
+        console.log(res);
+      },
+
+      fn2 () {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve(1);
+          });
+        });
+      }
+    }
+  }
+</script>
