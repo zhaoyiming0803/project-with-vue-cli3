b20618040ce39f61fb56170c1a6d92004dc999e2
zymseo <1047832475@qq.com>
2019-08-30 19:07:25 +0800

    test: v-slot

---
+++ src/components/CustomerDialog.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div>
+      <slot name="header"></slot>
+    </div>
+    <slot name="items">
+      <div v-for="(user, index) in items" :key="index">
+        <span>{{user.id}}</span>
+        <span>{{user.name}}</span>
+      </div>
+    </slot>
+    <div>
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    }
+  },
+
+  data () {
+    return {
+
+    }
+  },
+
+  mounted () {
+    
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,21 +2,40 @@
   <div class="about">
     <h1>This is an about page</h1>
     <van-switch v-model="checked"></van-switch>
+    <customer-dialog :items="items">
+      <template v-slot:header>
+        <div>header</div>
+      </template>
+      <template v-slot:items="slotProps">
+        <div>123</div>
+      </template>
+      <template v-slot:footer>
+        <div>footer</div>
+      </template>
+    </customer-dialog>
   </div>
 </template>
 
 <script scoped>
   import { Switch } from 'vant';
+  import CustomerDialog from '@/components/CustomerDialog.vue';
 
   export default {
     data () {
       return {
-        checked: false
+        checked: false,
+        items: [
+          {id: 1, name: 'zhangsan'},
+          {id: 2, name: 'lisi'},
+          {id: 3, name: 'wangwu'},
+          {id: 4, name: 'zhaoliu'}
+        ]
       }
     },
 
     components: {
-      [Switch.name]: Switch
+      [Switch.name]: Switch,
+      CustomerDialog
     },
 
     methods: {
